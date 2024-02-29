461df8889537e70faec1fec223ead43fcc660514
Alex-Sibiriu <alexsibiriu@protonmail.com>
2024-03-01 00:59:56 +0100

    [add] Player victory & Reorganized code

--- js/script.js
+++ js/script.js
@@ -1,21 +1,31 @@
 const grid = document.getElementById('grid-container');
 const btnStart = document.getElementById('btn-start');
 const btnRestart = document.getElementById('btn-restart');
-const difficulty = document.getElementById('difficulty').value;
+let maxSquares;
 
 btnStart.addEventListener('click', startGame);
 btnRestart.addEventListener('click', startGame);
 
 grid.append(btnStart);
 
-///////// FUNCTIONS
+/**************
+  FUNCTIONS
+**************/
+// Funzione di avvio gioco
 function startGame() {
   reset();
   
   const difficulty = document.getElementById('difficulty').value;
-  
-  let maxSquares;
 
+  setMaxSquares(difficulty);
+  createGrid(difficulty);
+
+  btnStart.classList.add('d-none');
+  btnRestart.classList.remove('disable');
+}
+
+// Funzione per impostare il numero di quadrati
+function setMaxSquares(difficulty) {
   if (difficulty === 'easy-grid') {
     maxSquares = 49;
   } else if (difficulty === 'medium-grid') {
@@ -23,35 +33,48 @@ function startGame() {
   } else {
     maxSquares = 100;
   }
-
-  for (i = 1; i <= maxSquares; i++) {
-    btnStart.classList.add('d-none')
-    const square = createSquare(i);
-    
-    grid.classList.remove('easy-grid', 'medium-grid', 'hard-grid');
-    grid.classList.add(difficulty);
-    grid.append(square);
-  }
-
-  btnRestart.classList.remove('disable');
 }
 
+// Funzione per creare la griglia
+function createGrid(difficulty) {
+  grid.className = difficulty;
+  for (let i = 1; i <= maxSquares; i++) {
+    const square = createSquare(i);
+    grid.append(square);
+  }
+}
+
+// Funzione per creare i quadrati
 function createSquare(indexN) {
   const cell = document.createElement('div');
   cell.className = 'square';
 
   cell.addEventListener('click', function() {
-
     this.classList.toggle('clicked');
-
-    this.innerHTML = (this.classList.contains('clicked')) 
-                    ? this.innerHTML = indexN
-                    : this.innerHTML = '';
+    this.innerHTML = (this.classList.contains('clicked')) ? indexN : '';
+    checkWin();
   })
 
   return cell;
 }
 
+// Funzione per controllare se il giocatore ha vinto
+function checkWin() {
+  const allClickedSquares = document.querySelectorAll('.clicked');
+
+  if (allClickedSquares.length === maxSquares) printResult();
+}
+
+// Funzione per stampare il risultato
+function printResult() {
+  const message = document.createElement('div');
+  message.className = 'game-end';
+  message.classList.add('win');
+  message.innerHTML = 'Hai Vinto!';
+  grid.append(message);
+}
+
+// Funzione di reset della griglia
 function reset() {
   grid.innerHTML = '';
 }
